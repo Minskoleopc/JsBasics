7a47f6869f9a3de53c6a58b96dd9101e79e250b6
Minskoleopc <minskoleopc@gmail.com>
2019-12-28 09:42:25 +0530

    DayTwo

--- script.js
+++ script.js
@@ -1,134 +1,274 @@
-
-var firstName = "Chinmay";
-console.log("firstName");
+/*****************************
+* Variables and data types
+*/
+/*
+var firstName = 'John';
 console.log(firstName);
 
+var lastName = 'Smith';
+var age = 28;
 
-// Numbers Strings Boolean null Undefined 
-// Numbers --> Interger  , Decimal , Float 
+var fullAge = true;
+console.log(fullAge);
 
-// Boolean --> true or false 
+var job;
+console.log(job);
 
-// null 
+job = 'Teacher';
+console.log(job);
 
-// Strings
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and MArk';
+var if = 23;
+*/
 
-// Undefined 
 
-var isMarried = false;
-console.log(isMarried)
 
-if(isMarried){
-    console.log("John is Married");
-}else{
-    console.log("John is not Married")
+/*****************************
+* Variable mutation and type coercion
+*/
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+
+/*****************************
+* Basic operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yeahMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older tha John');
+var x;
+console.log(typeof x);
+*/
+
+
+
+/*****************************
+* Operator precedence
+*/
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+
+// More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x--;
+console.log(x);
+*/
+
+
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+GOOD LUCK 😀
+*/
+/*
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+*/
+
+
+
+/*****************************
+* If / else statements
+*/
+/*
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
 }
 
 
-// Type coersion 
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
 
-// let name = null ;
-// console.log(name);
-// name = "chinmay";
-// console.log(name);
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
 
+var massJohn = 92;
+var heightJohn = 1.95;
 
-var name = "chinmay";
-var age = 29 ;
-console.log(age + 78 + name);
-console.log(name + (age + 78));
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
-
-
-// Basics 
-
-console.log(7+8);
-console.log(7-8);
-console.log(7*8);
-console.log(7/8);
-console.log(7%8);
-console.log(7 > 8);
-console.log(7 < 8);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
-// Lecture: Scoping
-
-
-// First scoping example
-
-/*
-var a = 'Hello!';
-first();
-
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Marks\'s.');
 }
 */
 
 
 
-// Example to show the differece between execution stack and scope chain
-
+/*****************************
+* Boolean logic
+*/
 /*
-var a = 'Hello!';
-first();
+var firstName = 'John';
+var age = 20;
 
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
-
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man.');
 }
 */
 
 
 
-///////////////////////////////////////
-// Lecture: The this keyword
+/*****************************
+* The Ternary Operator and Switch Statements
+*/
+/*
+var firstName = 'John';
+var age = 14;
+
+// Ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+(if (age >= 18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
